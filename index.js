9d787a9651435a1f1cfdade5c16081bd0efc2a97
OmarLirachi <612110332@utsalamanca.edu.mxm>
2023-01-25 10:19:44 -0600

    Cambios en Index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
@@ -10,6 +11,13 @@ app.use(bodyparser.urlencoded({
     extended: false
 }))
 app.use(bodyparser.json())
+
+//config CORS
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 //Conexión a BD
 const url = `mongodb+srv://roma:roma@cluster0.pv28ecj.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
